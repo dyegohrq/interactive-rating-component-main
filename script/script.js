35a65360ba5e4bfdd3d6fba3217f13f54b0b7820
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-02 10:56:53 -0300

    Tentando aplicar o script

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
-const mainContainer = document.getElementById(main-container)
-const ThankYouContainer = document.getElementById(ThankYou)
-const assessment = document.getElementById(bnt)
+const mainContainer = document.getElementById('main-container')
+const ThankYouContainer = document.getElementById('ThankYou')
+const assessment = document.querySelector('.bnt');
 const bntSubmit = document.querySelector('.bnt-submit')
-const ratingSpan = document.getElementById(ratingS)
+const ratingSpan = document.getElementById('ratingS')
 
+bntSubmit.addEventListener("click", () => {
+    mainContainer.style.display = "none"
+    ThankYouContainer.classList.remove = "hidden"
 
+    assessment.forEach(() => {
+        
+    });
+})
 
 
 
