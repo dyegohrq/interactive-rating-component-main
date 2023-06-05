53c601e1e5230faab73991671b24b6f9433a1e51
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-05 10:22:12 -0300

    style.js

--- script/script.js
+++ script/script.js
@@ -1,17 +1,24 @@
-const mainContainer = document.getElementById('main-container')
-const ThankYouContainer = document.getElementById('ThankYou')
-const assessment = document.querySelector('.bnt');
-const bntSubmit = document.querySelector('.bnt-submit')
+const mainContainer = document.querySelector('.main-container')
+const ThankYouContainer = document.querySelectorAll('.ThankYou')
+const assessment = document.querySelectorAll('.bnt')
+const submitButton = document.getElementById('submit-rating')
+const rateAgain = document.getElementById('rate-again')
 const ratingSpan = document.getElementById('ratingS')
 
-bntSubmit.addEventListener("click", () => {
+submitButton.addEventListener("click", () => {
     mainContainer.style.display = "none"
-    ThankYouContainer.classList.remove = "hidden"
-
-    assessment.forEach(() => {
-        
-    });
+    ThankYouContainer.classList.remove("hidden")
 })
+
+rateAgain.addEventListener("click")
+
+
+
+
+
+
+
+
 
 
 
