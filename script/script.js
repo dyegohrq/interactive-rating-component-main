4a2b6f4d9d748f44b0d21eabd43913a77ce2e749
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-06 06:40:05 -0300

    dando inicio ao projeto, de novo.

--- script/script.js
+++ script/script.js
@@ -1,16 +1,4 @@
-const mainContainer = document.querySelector('.main-container')
-const ThankYouContainer = document.querySelectorAll('.ThankYou')
-const assessment = document.querySelectorAll('.bnt')
-const submitButton = document.getElementById('submit-rating')
-const rateAgain = document.getElementById('rate-again')
-const ratingSpan = document.getElementById('ratingS')
 
-submitButton.addEventListener("click", () => {
-    mainContainer.style.display = "none"
-    ThankYouContainer.classList.remove("hidden")
-})
-
-rateAgain.addEventListener("click")
 
 
 
