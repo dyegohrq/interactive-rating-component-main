aea7c6342a462e345c3f6a5bf6a03e720ab95857
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-06 07:56:54 -0300

    quase finalizando o projeto.
    Adicionando o script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
+const mainContainer = document.querySelector(".main-container")
+const thankContainer = document.querySelector(".thank-container")
+const submitButton = document.getElementById("submit-button")
+const rateButton = document.getElementById("rate-button")
+
+submitButton.addEventListener("click", () => {
+    mainContainer.style.display = "none"
+    thankContainer.classList.remove("hidden")
+
+    
+})
+
+rateButton.addEventListener("click", () => {
+    mainContainer.style.display = "block"
+    thankContainer.classList.add("hidden")
+})
+
+
+
 
 
 
