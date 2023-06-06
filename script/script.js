4bc805f61f221cae4550a95ba42fac0bd6ade4db
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-06 08:17:32 -0300

    finalizei o projeto.

--- script/script.js
+++ script/script.js
@@ -2,13 +2,21 @@ const mainContainer = document.querySelector(".main-container")
 const thankContainer = document.querySelector(".thank-container")
 const submitButton = document.getElementById("submit-button")
 const rateButton = document.getElementById("rate-button")
+const ratingBnt = document.querySelectorAll(".bnt")
+const actualRating = document.getElementById("rating")
 
 submitButton.addEventListener("click", () => {
     mainContainer.style.display = "none"
     thankContainer.classList.remove("hidden")
 
-    
+    ratingBnt.forEach((ratings) => {
+        ratings.addEventListener("click", () => {
+            actualRating.innerHTML = ratings.innerHTML
+        })
+    })
 })
+
+ 
 
 rateButton.addEventListener("click", () => {
     mainContainer.style.display = "block"
